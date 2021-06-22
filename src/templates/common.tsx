fe070cf8b9bc4718c7a64a281b97f988e21568c5
heppokofrontend <heppokofrontend@gmail.com>
2021-06-23 04:06:21 +0900

    :zap: normalize.css

--- src/templates/common.tsx
+++ src/templates/common.tsx
@@ -5,6 +5,8 @@ import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { Link } from "gatsby"
 import { YouTube } from '../components/YouTube'
+import '../../node_modules/normalize.css/normalize.css'
+import '../common.scss'
 
 const shortcodes = { Link, YouTube } // Provide common components here
 
