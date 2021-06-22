5640b298fbe6531df53ec0a0b32548af5af1dc7a
heppokofrontend <heppokofrontend@gmail.com>
2021-06-23 08:59:51 +0900

    :zap: Update

--- src/components/YouTube.tsx
+++ src/components/YouTube.tsx
@@ -11,8 +11,7 @@ export const YouTube: Component  = ({ src, width, height }) => {
   // https://www.youtube.com/embed/jB48OrSwapc?start=1
   // https://www.youtube.com/watch?v=jB48OrSwapc
   // TODO: 複数のパターンを許容するか（シェアURLを前提）
-  const _src = src.replace(`https://youtu.be/`, `https://www.youtube.com/embed/`)
-    .replace(`?t=`, `?start=`)
+  const _src = src.replace(`https://youtu.be/`, `https://www.youtube.com/embed/`).replace(`?t=`, `?start=`)
 
   return (
     <iframe width={width ?? 560} height={height ?? 315} src={_src} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
