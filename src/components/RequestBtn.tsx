c7f72189cc3d0bd583c9f57dd0f354794c714d16
heppokofrontend <heppokofrontend@gmail.com>
2021-06-30 04:58:11 +0900

    :zap: Update

--- src/components/RequestBtn.tsx
+++ src/components/RequestBtn.tsx
@@ -4,7 +4,7 @@ import * as styles from './RequestBtn.module.scss'
 export function RequestBtn() {
   return (
     <p className={styles.wrap}>
-      <a href="https://github.com/Kiryu-kai/aphorism/issues/new?assignees=&amp;labels=request&amp;template=-------------.md&amp;title=%E4%BC%9A%E9%95%B7%E3%80%8C+%E2%97%AF%E2%97%AF%E2%97%AF+%E3%80%8D" className={styles.link}>
+      <a href="https://github.com/Kiryu-kai/aphorism/issues/new?assignees=heppokofrontend&amp;labels=request&amp;template=-------------.md&amp;title=%E4%BC%9A%E9%95%B7%E3%80%8C+%E2%97%AF%E2%97%AF%E2%97%AF+%E3%80%8D" className={styles.link}>
         新しい格言をリクエストする 🎉
       </a>
     </p>
