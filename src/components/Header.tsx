f33f691948099544362f2cbba8beba4ed8bc2ee9
heppokofrontend <heppokofrontend@gmail.com>
2021-06-30 03:08:52 +0900

    :zap: ヘッダを調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,17 +6,19 @@ import youtube from '../images/header.jpeg'
 export function Header () {
   return (
     <header className={styles.header}>
-      <h1 className={styles.h1}>
-        <Link to="/" className={styles.h1__link}>
-          #桐生ココの格言
-        </Link>
-      </h1>
+      <div className={styles.inner}>
+        <h1 className={styles.h1}>
+          <Link to="/" className={styles.h1__link}>
+            #桐生ココの格言
+          </Link>
+        </h1>
 
-      <p className={styles.youtube}>
-        <a href="https://www.youtube.com/channel/UCS9uQI-jC3DE0L4IpXyvr6w" className={styles.youtube__link}>
-          <img src={youtube} alt="Coco Ch. 桐生ココ" className={styles.youtube__img} />
-        </a>
-      </p>
+        <p className={styles.youtube}>
+          <a href="https://www.youtube.com/channel/UCS9uQI-jC3DE0L4IpXyvr6w" className={styles.youtube__link}>
+            <img src={youtube} alt="Coco Ch. 桐生ココ" className={styles.youtube__img} />
+          </a>
+        </p>
+      </div>
     </header>
   )
 }
